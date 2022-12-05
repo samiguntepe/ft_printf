b9d4eb770fb0fbc1165169fe26f0ddee31881e7e
Sami Guntepe <sguntepe@k1m14s03.42kocaeli.com.tr>
2022-12-05 15:35:27 +0300

    42

--- ft_printf.h
+++ ft_printf.h
@@ -6,21 +6,23 @@
 /*   By: sguntepe <@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 10:30:20 by sguntepe          #+#    #+#             */
-/*   Updated: 2022/12/04 15:40:53 by sguntepe         ###   ########.fr       */
+/*   Updated: 2022/12/05 15:27:48 by sguntepe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+# include <stdio.h>
+# include <unistd.h>
+# include <stdbool.h>
 # include <stdarg.h>
-# include "libft/libft.h"
 
-int	ft_putchar(int c);
-int	ft_string(char *str);
-int	ft_int(int x);
-int	ft_format(char c, va_list arg);
-int	ft_ptr(unsigned long a, int sign);
-int	ft_unsigned(unsigned int a);
+int	ft_printf(const char *str, ...);
+int	ft_format(va_list args, char c);
+int	ft_int(int a);
 int	ft_hex(unsigned int a, char c);
+int	ft_point(unsigned long a, int sign);
+int	ft_string(char *str);
+int	ft_unsigned(unsigned int a);
 
 #endif
--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,65 +6,28 @@
 /*   By: sguntepe <@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 15:23:41 by sguntepe          #+#    #+#             */
-/*   Updated: 2022/12/04 17:48:01 by sguntepe         ###   ########.fr       */
+/*   Updated: 2022/12/05 15:16:47 by sguntepe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_string(char *str)
-{
-	size_t	i;
-
-	i = 0;
-	if (!str)
-		return (write(1, "\0", 1));
-	while (str[i])
-	{
-		write(1, &str[i], 1);
-		i++;
-	}
-	return (i);
-}
-
-int	ft_int(int x)
-{
-	size_t	i;
-	char	*p_int;
-
-	i = 0;
-	p_int = ft_itoa(x);
-	while (p_int[i])
-	{
-		ft_putchar(i);
-		i++;
-	}
-	return (i);
-}
-
-int	ft_ptr(unsigned long a, int sign)
+int	ft_int(int a)
 {
 	int	ret;
 
 	ret = 0;
-	if (sign == 1)
+	if (a == 0)
+		return (write(1, "0", 1));
+	if (a == -2147483648)
+		return (write(1, "-2147483648", 11));
+	if (a < 0)
 	{
-		ret += write(1, "0x", 2);
-		sign = 0;
+		ret += write(1, "-", 1);
+		a *= -1;
 	}
-	if (a >= 16)
-		ret += ft_ptr(a / 16, 0);
-	write(1, &"0123456789abcdef"[a % 16], 1);
-	return (ret + 1);
-}
-
-int	ft_unsigned(unsigned int a)
-{
-	int	ret;
-
-	ret = 0;
 	if (a >= 10)
-		ret += ft_unsigned(a / 10);
+		ret += ft_int(a / 10);
 	write(1, &"0123456789"[a % 10], 1);
 	return (ret + 1);
 }
@@ -82,3 +45,43 @@ int	ft_hex(unsigned int a, char c)
 		write(1, &"0123456789abcdef"[a % 16], 1);
 	return (ret + 1);
 }
+
+int	ft_point(unsigned long a, int sign)
+{
+	int	ret;
+
+	ret = 0;
+	if (sign == 1)
+	{
+		ret += write(1, "0x", 2);
+		sign = 0;
+	}
+	if (a >= 16)
+		ret += ft_point(a / 16, 0);
+	write(1, &"0123456789abcdef"[a % 16], 1);
+	return (ret + 1);
+}
+
+int	ft_string(char *str)
+{
+	int	i;
+
+	i = -1;
+	if (!str)
+		return (write(1, "(null)", 6));
+	while (str[++i])
+		write(1, &str[i], 1);
+	return (i);
+}
+
+int	ft_unsigned(unsigned int a)
+{
+	int	ret;
+
+	ret = 0;
+	if (a >= 10)
+		ret += ft_unsigned(a / 10);
+	write(1, &"0123456789"[a % 10], 1);
+	return (ret + 1);
+}
+
